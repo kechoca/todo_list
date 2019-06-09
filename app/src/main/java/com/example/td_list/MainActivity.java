3af41a585f87962f5a18e18b8693c1d6c8703f7d
Paphawara Archewavanit <chote.paphawara@gmail.com>
2019-06-09 16:14:25 +0700

    test

--- app/src/main/java/com/example/td_list/MainActivity.java
+++ app/src/main/java/com/example/td_list/MainActivity.java
@@ -95,5 +95,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+
+        //test
     }
 }
